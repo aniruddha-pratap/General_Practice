9097f1545ccf6fe24a82ba548dd1758bdab93f84
aniruddha-pratap <aniruddhapratap.singh@sjsu.edu>
2018-03-04 21:39:57 -0800

    Baseball Game

--- src/Miscellaneous/BaseballGame.java
+++ src/Miscellaneous/BaseballGame.java
@@ -1,7 +1,37 @@
 package Miscellaneous;
 
+import java.util.Stack;
+
 public class BaseballGame {
 
+    public static int calPoints(String[] ops) {
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (String s : ops) {
+            if (s.equals("C")) sum -= stack.pop();
+            else if (s.equals("D")) {
+                stack.push(stack.peek() * 2);
+                sum += stack.peek();
+            }
+            else if (s.equals("+")) {
+                int last = stack.pop();
+                int add = last + stack.peek();
+                stack.push(last);
+                stack.push(add);
+                sum += stack.peek();
+            }
+            else {
+                stack.push(Integer.parseInt(s));
+                sum += stack.peek();
+            }
+        }
+        return sum;
+    }
 
+
+    public static void main(String[] args) {
+        String s[] = {"C","2", "5", "D","+"};
+        System.out.println(BaseballGame.calPoints(s));
+    }
 
 }
